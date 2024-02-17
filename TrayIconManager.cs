ae258f12aff8be5242bef214c36e355571fc08e5
saasan <saasan.s2works@hotmail.com>
2024-02-17 15:18:23 +0900

    Add: 画像を削除時に通知する設定

--- TrayIconManager.cs
+++ TrayIconManager.cs
@@ -3,10 +3,19 @@ namespace RifClip;
 public class TrayIconManager
 {
     private readonly NotifyIcon notifyIcon = new();
+    private Settings settings;
 
-    public void Initialize()
+    public TrayIconManager(Settings settings)
     {
+        this.settings = settings;
         ContextMenuStrip contextMenu = new();
+
+        ToolStripMenuItem menuNotifyOnRemoveImage = new("画像を削除時に通知する(&N)");
+        menuNotifyOnRemoveImage.Click += MenuNotifyOnRemoveImage_Click;
+        menuNotifyOnRemoveImage.CheckState = settings.NotifyOnRemoveImage ? CheckState.Checked : CheckState.Unchecked;
+        contextMenu.Items.Add(menuNotifyOnRemoveImage);
+
+        contextMenu.Items.Add("-");
 
         contextMenu.Items.Add("終了(&X)", null, (sender, e) =>
         {
@@ -20,9 +29,19 @@ public class TrayIconManager
         notifyIcon.Visible = true;
     }
 
-    public void ShowBalloonTip(string? tipTitle, string? tipText, ToolTipIcon tipIcon = ToolTipIcon.Info) {
+    public void ShowBalloonTip(string? tipTitle, string? tipText, ToolTipIcon tipIcon = ToolTipIcon.Info)
+    {
         tipTitle = tipTitle ?? "";
         tipText = tipText ?? "";
         notifyIcon.ShowBalloonTip(1, tipTitle, tipText, tipIcon);
+    }
+
+    private void MenuNotifyOnRemoveImage_Click(object? sender, EventArgs e)
+    {
+        settings.NotifyOnRemoveImage = !settings.NotifyOnRemoveImage;
+
+        if (sender is null) return;
+        ToolStripMenuItem menu = (ToolStripMenuItem)sender;
+        menu.CheckState = settings.NotifyOnRemoveImage ? CheckState.Checked : CheckState.Unchecked;
     }
 }
